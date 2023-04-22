5c77f33256cd26ed2ec01bced0e58c5f03d2f8f3
Allen Go <goallen2001@gmail.com>
2023-04-22 12:42:13 -0700

    finished bread assignment day 5

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -2,14 +2,26 @@ const express = require('express')
 const breads = express.Router()
 const Bread = require('../models/bread.js')
 
+
 // INDEX
 breads.get('/', (req, res) => {
     res.render('Index',
       {
-        breads: Bread
+        breads: Bread,
+        title: 'Index Page'
       }
     )
-  // res.send(Bread)
+})
+
+// UPDATE
+breads.put('/:arrayIndex', (req, res) => {
+  if(req.body.hasGluten === 'on'){
+    req.body.hasGluten = true
+  } else {
+    req.body.hasGluten = false
+  }
+  Bread[req.params.arrayIndex] = req.body
+  res.redirect(`/breads/${req.params.arrayIndex}`)
 })
 
 // NEW
@@ -17,14 +29,23 @@ breads.get('/new', (req, res) => {
   res.render('new')
 })
 
+// EDIT
+breads.get('/:indexArray/edit', (req, res) => {
+  res.render('edit', {
+    bread: Bread[req.params.indexArray],
+    index: req.params.indexArray
+  })
+})
+
 // SHOW
 breads.get('/:arrayIndex', (req, res) => {
   if (Bread[req.params.arrayIndex]) {
     res.render('Show', {
-      bread:Bread[req.params.arrayIndex]
+      bread:Bread[req.params.arrayIndex],
+      index: req.params.arrayIndex
     })
   } else {
-    res.send('404')
+    res.render('error404')
   }
 })
 
@@ -41,5 +62,13 @@ breads.post('/', (req, res) => {
   Bread.push(req.body)
   res.redirect('/breads')
 })
+
+// DELETE
+breads.delete('/:indexArray', (req, res) => {
+  Bread.splice(req.params.indexArray, 1)
+  res.status(303).redirect('/breads')
+})
+
+
 
 module.exports = breads
--- views/show.jsx
+++ views/show.jsx
@@ -1,35 +1,32 @@
 const React = require('react')
 const Default = require('./layouts/default')
 
-function Show ({bread}) {
+
+function Show ({bread, index}) {
   console.log(bread.name)
     return (
       <Default>
         <h2>Show Page</h2>
+        <h3>{bread.name}</h3>
+        <p>
+            and it
+            {
+                bread.hasGluten
+                ? <span> does </span>
+                : <span> does NOT </span>
+            }
+            have gluten.
+        </p>
+        <img src={bread.image} alt={bread.name} />
+        <li><a href="/breads">Go Home</a></li>
+        
+        <form action={`/breads/${index}?_method=DELETE`} method="POST">
+          <input type='submit' value="DELETE"/>
+        </form>
+        <a href={`/breads/${index}/edit`}><button>Edit</button></a>
+
       </Default>
     )
 }
-
-function Show ({bread}) {
-    // Confirm we are getting our bread data in the terminal.
-    // console.log(bread.name)
-      return (
-        <Default>
-            <h3>{bread.name}</h3>
-            <p>
-                and it
-                {
-                    bread.hasGluten
-                    ? <span> does </span>
-                    : <span> does NOT </span>
-                }
-                have gluten.
-            </p>
-            <img src={bread.image} alt={bread.name} />
-            <li><a href="/breads">Go home</a></li>
-        </Default>
-      )
-}
-  
 
 module.exports = Show
